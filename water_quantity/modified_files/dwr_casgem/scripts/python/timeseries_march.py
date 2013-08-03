62b8006c24bb25baab82ceab99772add8b2bf19d
Chacha Sikes <chachasikes@gmail.com>
2013-08-02 23:47:30 -0700

    Added python script to just extract well readings in march. removed no data csv files

---
+++ water_quantity/modified_files/dwr_casgem/scripts/python/timeseries_march.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python
+import sys, os, datetime
+
+# Count total records.
+totalcount = 0
+foundcount = 0
+
+# Create list of files.
+file_list = []
+
+# Set path to list of files.
+path = "../../timeseries/"
+# @TODO Alternately, take argument from command row
+# print "command row argument is", sys.argv[1]
+
+# Open file we will write to, replace it with new data.
+fout=open("../../database/casgem_timeseries_march.csv","r+")
+fout.seek(0)
+
+# Read each file in the directory.
+for file in [doc for doc in os.listdir(path)
+
+# Build list of file names in the directory.
+if doc.endswith(".csv")]:
+    file_list.append(file)
+
+# Test if string represents an integer.
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+# Return time file was created.
+def modificationDate(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
+
+# Process a row, adding extra handling to remove errors in mapping.
+def cleanupRow(row):
+
+    month = ""
+    try: 
+        # Split row into array based on comma.
+        row_list = row.split(",")
+    
+        # Create empty values for month, day, year
+        date_csv = ",,"
+
+    
+        # Split the / separated date and convert into comma-separated string.
+        if(row_list[8] != '' and row_list[8] != "Date"):
+
+            date = row_list[8].strip()
+            date_list = date.split("/")
+
+            month = date_list[0]
+            #print month
+            date_csv = ",".join(date_list)
+    
+        # Error checking for null and empty values. Convert to empty string or NULL value.
+        if(row_list[10] == ''):
+            row_list[10] = "" #No Measurement
+    
+        if(row_list[11] == ''):
+            row_list[11] = "" #Questionable Measurement
+    
+        if(row_list[12] == ''):
+            row_list[12] = 'NULL' #Reading @RP
+    
+        if(row_list[13] == ''):
+            row_list[13] = 'NULL' #Reading @WS
+    
+        if(row_list[14] == ''):
+            row_list[14] = 'NULL' #RP to WS
+    
+        if(row_list[15] == ''):
+            row_list[15] = 'NULL' #RP Elevation
+    
+        if(row_list[16] == ''):
+            row_list[16] = 'NULL' #GS Elevation
+    
+        if(row_list[17] == ''):
+            row_list[17] = 'NULL' #WSE
+    
+        if(row_list[18] == ''):
+            row_list[18] = 'NULL' #GS to WS
+        
+        # Put array back together
+        row = ",".join(row_list)
+    
+        # Append date.
+        row = row + date_csv
+        
+        if(month == '3'):
+            return row
+        else:
+            return "\n"
+    
+    except ValueError:
+        return ""
+
+# Read file list.
+for i in file_list:
+
+    print "Processing file: " + path + i
+
+    # Open file
+    f = open(path + i, 'r')
+    filetime = modificationDate(path + i)
+
+    filecount = 0
+    rowcount = 0
+
+    # Read each row, perform cleanup functions.
+    for row in f:
+        # @TODO Properly escape empty files. Deleted no data files from the directory for now.
+        if row != '':
+            # Remove whitespace
+            row = row.strip()
+
+            # Create placeholder names for columnn header rows.
+            if rowcount == 0:
+                filename = "filename"
+                latitude = "latitude"
+                longitude = "longitude"
+                county = "county"      
+                well = "well"
+
+            else:
+
+                filename = str(i)
+
+                county = i.split("_", 1)[0]
+    
+                # Parse latitude and longitude
+                # 397135N1219039W001
+                location = row.split(",")[0]
+                latitude = location[0:6]
+                if(RepresentsInt(latitude)):
+                    latitude = float(latitude) * 0.0001
+                longitude = location[7:14]
+                if(RepresentsInt(longitude)):
+                    longitude = float(longitude)  * -0.0001
+                
+                #W001
+                well = ""
+                if location[14:15] == 'W':
+                    well = location[14:18]
+                else:
+                    well = ""
+
+            # Build row.
+            row = filename + "," + county + "," + str(latitude) + "," + str(longitude) + "," + well + "," + str(filetime) + "," + row
+
+            # Resplit row, clean up columns, add date facets.
+            row_dated = cleanupRow(row) + "\n"
+
+            # Write row to output file.
+            if filecount != 0:
+                if rowcount != 0:
+                    if len(row_dated) > 2:
+                        foundcount = foundcount + 1
+                        fout.write(row_dated)
+
+            # Add cleaned up column headers.
+            else:
+                row = "filename,county,latitude,longitude,well,filetime,casgem_id,local_well_number,date,military_time_pst,no_measurement,questionable_measurement,reading_ap,reading_ws,rp_to_ws,rp_elecation,gs_elevation,wse,gs_to_ws,measurement_method,measurement_accuracy,collecting,comments,day,month,year"
+
+                fout.write(row + '\n')
+
+            rowcount = rowcount + 1
+            totalcount = totalcount + 1
+        filecount = filecount + 1
+
+fout.truncate()
+fout.close()
+print "Done. Processed " + str(totalcount) + " records. Found " + str(foundcount) + "."
+sys.exit()
