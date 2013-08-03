6797fc20a2b57db1c0b12f40101148942a832657
Chacha Sikes <chachasikes@gmail.com>
2013-08-02 22:56:22 -0700

    Added comments, cleaned up file.

--- water_quantity/modified_files/dwr_casgem/scripts/python/timeseries_csv.py
+++ water_quantity/modified_files/dwr_casgem/scripts/python/timeseries_csv.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python
 import sys, os, datetime
- 
+
+# Create list of files.
 file_list = []
  
-#print "command line argument is", sys.argv[1]
-path = "../../timeseries_test/"
 
+# Set path to list of files.
+path = "../../timeseries_test/"
+# @TODO Alternately, take argument from command row
+# print "command row argument is", sys.argv[1]
+
+# Open file we will write to, replace it with new data.
 fout=open("../../database/casgem_timeseries.csv","r+")
 fout.seek(0)
 
+# Read each file in the directory.
 for file in [doc for doc in os.listdir(path)
 
+# Build list of file names in the directory.
 if doc.endswith(".csv")]:
     file_list.append(file)
 
+# Test if string represents an integer.
 def RepresentsInt(s):
     try: 
         int(s)
@@ -21,106 +29,135 @@ def RepresentsInt(s):
     except ValueError:
         return False
 
-def modification_date(filename):
+# Return time file was created.
+def modificationDate(filename):
     t = os.path.getmtime(filename)
     return datetime.datetime.fromtimestamp(t)
 
+# Process a row, adding extra handling to remove errors in mapping.
+def cleanupRow(row):
 
-def cleanupLine(line):
-    line_list = line.split(",")
+    # Split row into array based on comma.
+    row_list = row.split(",")
+
+    # Create empty values for month, day, year
     date_csv = ",,,"
-    if(line_list[8] != '' and line_list[8] != "Date"):
-        date = line_list[8].strip()
+
+    # Split the / separated date and convert into comma-separated string.
+    if(row_list[8] != '' and row_list[8] != "Date"):
+        date = row_list[8].strip()
         date_list = date.split("/")
         date_csv = ",".join(date_list)
 
-    if(line_list[10] == ''):
-        line_list[10] = "" #No Measurement
+    # Error checking for null and empty values. Convert to empty string or NULL value.
+    if(row_list[10] == ''):
+        row_list[10] = "" #No Measurement
 
-    if(line_list[11] == ''):
-        line_list[11] = "" #Questionable Measurement
+    if(row_list[11] == ''):
+        row_list[11] = "" #Questionable Measurement
 
-    if(line_list[12] == ''):
-        line_list[12] = 'NULL' #Reading @RP
+    if(row_list[12] == ''):
+        row_list[12] = 'NULL' #Reading @RP
 
-    if(line_list[13] == ''):
-        line_list[13] = 'NULL' #Reading @WS
+    if(row_list[13] == ''):
+        row_list[13] = 'NULL' #Reading @WS
 
-    if(line_list[14] == ''):
-        line_list[14] = 'NULL' #RP to WS
+    if(row_list[14] == ''):
+        row_list[14] = 'NULL' #RP to WS
 
-    if(line_list[15] == ''):
-        line_list[15] = 'NULL' #RP Elevation
+    if(row_list[15] == ''):
+        row_list[15] = 'NULL' #RP Elevation
 
-    if(line_list[16] == ''):
-        line_list[16] = 'NULL' #GS Elevation
+    if(row_list[16] == ''):
+        row_list[16] = 'NULL' #GS Elevation
 
-    if(line_list[17] == ''):
-        line_list[17] = 'NULL' #WSE
+    if(row_list[17] == ''):
+        row_list[17] = 'NULL' #WSE
 
-    if(line_list[18] == ''):
-        line_list[18] = 'NULL' #GS to WS
+    if(row_list[18] == ''):
+        row_list[18] = 'NULL' #GS to WS
     
+    # Put array back together
+    row = ",".join(row_list)
 
-    line = ",".join(line_list)
-    line = line + "," + date_csv
+    # Append date.
+    row = row + "," + date_csv
     
-    return line
+    return row
 
+# Count total records.
 totalcount = 0
+
+# Read file list.
 for i in file_list:
+
     print "Processing file: " + path + i
+
+    # Open file
     f = open(path + i, 'r')
+
     filecount = 0
-    linecount = 0
-    filetime = modification_date(path + i)
-    for line in f:
-        if line != '':
-            line = line.strip()
-            #print filecount
-            if linecount == 0:
+    rowcount = 0
+
+    filetime = modificationDate(path + i)
+
+    # Read each row, perform cleanup functions.
+    for row in f:
+        if row != '':
+            # Remove whitespace
+            row = row.strip()
+
+            # Create placeholder names for columnn header rows.
+            if rowcount == 0:
                 filename = "filename"
                 latitude = "latitude"
                 longitude = "longitude"
                 county = "county"      
                 well = "well"
                 filetime = "filetime"
+
             else:
+
                 filename = str(i)
 
                 county = i.split("_", 1)[0]
     
+                # Parse latitude and longitude
                 # 397135N1219039W001
-                location = line.split(",")[0]
+                location = row.split(",")[0]
                 latitude = location[0:6]
                 if(RepresentsInt(latitude)):
                     latitude = float(latitude) * 0.0001
                 longitude = location[7:14]
                 if(RepresentsInt(longitude)):
                     longitude = float(longitude)  * -0.0001
-    
+                
+                #W001
                 well = ""            
                 if location[14:15] == 'W':
                     well = location[14:18]
                 else:
                     well = ""
 
-            line = filename + "," + county + "," + str(latitude) + "," + str(longitude) + "," + well + "," + str(filetime) + "," + line
-            line_dated = cleanupLine(line) + "\n"
+            # Build row.
+            row = filename + "," + county + "," + str(latitude) + "," + str(longitude) + "," + well + "," + str(filetime) + "," + row
 
+            # Resplit row, clean up columns, add date facets.
+            row_dated = cleanupRow(row) + "\n"
+
+            # Write row to output file.
             if filecount != 0:
-                if linecount != 0:
-                    fout.write(line_dated)
+                if rowcount != 0:
+                    fout.write(row_dated)
+
+            # Add cleaned up column headers.
             else:
-                line = "filename,county,latitude,longitude,well,filetime,casgem_id,local_well_number,date,military_time_pst,no_measurement,questionable_measurement,reading_ap,reading_ws,rp_to_ws,rp_elecation,gs_elevation,wse,gs_to_ws,measurement_method,measurement_accuracy,collecting,comments,month,day,year"
+                row = "filename,county,latitude,longitude,well,filetime,casgem_id,local_well_number,date,military_time_pst,no_measurement,questionable_measurement,reading_ap,reading_ws,rp_to_ws,rp_elecation,gs_elevation,wse,gs_to_ws,measurement_method,measurement_accuracy,collecting,comments,month,day,year"
 
-                fout.write(line + '\n')
-                
-                #print line
+                fout.write(row + '\n')
 
-            linecount = linecount + 1
+            rowcount = rowcount + 1
             totalcount = totalcount + 1
-
         filecount = filecount + 1
 
 fout.truncate()
